671e66fd1a964cfb094411596eb63d945f2ca58c
minmin9324 <minmin93240@gmail.com>
2021-10-24 17:58:55 +0900

    try

---
+++ src/pages/Main.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Main = () => {
+  return <div>"Hello"</div>;
+};
+
+export default Main;
---
+++ src/pages/Write.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Write = () => {
+  return <div>"Helloasdf"</div>;
+};
+
+export default Write;
